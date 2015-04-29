0d009a80b6f1f5d661a2f4b79ee2c699053f3d82
mrodrigues <gmrodrigues@gmail.com>
2015-04-29 20:54:33 +0100

    Added sprint boot Application initializer

--- src/main/java/com/enthalpystudios/Application.java
+++ src/main/java/com/enthalpystudios/Application.java
@@ -1,7 +1,12 @@
 package com.enthalpystudios;
 
-/**
- * Created by mrodrigues on 29/04/2015.
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
