28855b2e7c257f3e58210d0bcae14f07ab3dc5ee
Sonia Fernandez <i42feros@gmail.com>
2015-06-21 22:05:58 +0100

    Included integration tests

--- src/main/java/com/enthalpystudios/Application.java
+++ src/main/java/com/enthalpystudios/Application.java
@@ -1,9 +1,15 @@
 package com.enthalpystudios;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
